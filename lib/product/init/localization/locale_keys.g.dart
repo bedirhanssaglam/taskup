02b86a985e0a0fef5910b2d2d054d6e830255648
Bedirhan Saglam <bedirhansaglam270@gmail.com>
2024-09-27 23:43:20 +0300

    add scripts

--- lib/product/init/localization/locale_keys.g.dart
+++ lib/product/init/localization/locale_keys.g.dart
@@ -1,21 +1,18 @@
 // DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
 
-// ignore_for_file: constant_identifier_names
-
-abstract class LocaleKeys {
+abstract class  LocaleKeys {
   static const hello = 'hello';
   static const error_failedToUsers = 'error.failedToUsers';
   static const error_unknownException = 'error.unknownException';
   static const error_userDisabled = 'error.userDisabled';
   static const error_invalidEmail = 'error.invalidEmail';
-  static const error_signUp_emailAlreadyInUse =
-      'error.signUp.emailAlreadyInUse';
-  static const error_signUp_operationNotAllowed =
-      'error.signUp.operationNotAllowed';
+  static const error_signUp_emailAlreadyInUse = 'error.signUp.emailAlreadyInUse';
+  static const error_signUp_operationNotAllowed = 'error.signUp.operationNotAllowed';
   static const error_signUp_weakPassword = 'error.signUp.weakPassword';
   static const error_signUp = 'error.signUp';
   static const error_login_userNotFound = 'error.login.userNotFound';
   static const error_login_wrongPassword = 'error.login.wrongPassword';
   static const error_login = 'error.login';
   static const error = 'error';
+
 }
---
+++ scripts/lang.sh
@@ -0,0 +1,1 @@
+dart run easy_localization:generate -O lib/product/init/localization -f keys -o locale_keys.g.dart -S assets/translations
