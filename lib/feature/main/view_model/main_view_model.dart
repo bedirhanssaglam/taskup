bd0b228894f36ffa19df3cc5d25575b77042eee3
Bedirhan Saglam <bedirhansaglam270@gmail.com>
2024-09-29 00:22:13 +0300

    test: add main view model tests

--- lib/feature/main/view_model/main_view_model.dart
+++ lib/feature/main/view_model/main_view_model.dart
@@ -2,16 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class MainViewModel extends StateNotifier<int> {
-  MainViewModel() : super(0);
+  MainViewModel() : super(0) {
+    pageController = PageController();
+  }
 
-  final PageController pageController = PageController();
+  late final PageController pageController;
 
   void changePage(int index) {
     state = index;
-    pageController.jumpToPage(index);
+    if (pageController.hasClients) pageController.jumpToPage(index);
   }
 
   void reset() {
     state = 0;
+    if (pageController.hasClients) pageController.jumpToPage(0);
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
   }
 }
